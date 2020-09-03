45ac59c34c9fbd9fd13db908d5f5d885cd4fd10e
Владислав Смолькин <@college.edu>
2020-09-03 14:31:44 +0300

    z

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-      <a href="#"><img src="@/assets/user.png"></a>
-      <a href="#"><img src="@/assets/quill-drawing-a-line.png">BestCaseScenario</a>
-      <a class="link" href="#">/home</a>
-      <a class="link" href="#">/curriculum</a>
-      <a class="link" href="#">/about</a>
+  <div class="menu">
+      <a class="username" href="#"><img src="@/assets/user.png"></a>
+      <a class="title" href="#"><img src="@/assets/quill-drawing-a-line.png">BestCaseScenario</a>
+      <div class="links">
+        <a href="#">/home</a>
+        <a href="#">/curriculum</a>
+        <a href="#">/about</a>
+      </div>
   </div>
 </template>
 
@@ -15,5 +17,30 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "../styles.scss";
 
+.menu {
+  background: $primary;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  color: $secondary;
+  align-items: center;
+
+  a {
+    text-decoration: none;
+    &:visited {
+      color: $secondary;
+    }
+  }
+
+  .title {
+    font-size: 32px;
+  }
+}
+
+img {
+  width: 32px;
+}
 </style>

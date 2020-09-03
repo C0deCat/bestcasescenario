7d86afa95ee4da433d8a9be385c77ca0e935a8ce
Владислав Смолькин <@college.edu>
2020-09-03 12:46:50 +0300

    start app

---
+++ src/components/navigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <a href="#"><img src="@/assets/user.png"></a>
+      <a href="#"><img src="@/assets/quill-drawing-a-line.png">BestCaseScenario</a>
+      <a class="link" href="#">/home</a>
+      <a class="link" href="#">/curriculum</a>
+      <a class="link" href="#">/about</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>

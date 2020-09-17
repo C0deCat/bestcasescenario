94c04ed9a052490c772bcee696d710b8723fdfad
Владислав Смолькин <@college.edu>
2020-09-17 14:41:11 +0300

    Had conection with firebase

--- src/components/authform.vue
+++ src/components/authform.vue
@@ -1,15 +1,35 @@
 <template>
-  <form>
-      <input type="email" id="email"><div class="break"></div>
-      <input type="password" id="password"><div class="break"></div>
-      <input type="password" id="confirm_pass"><div class="break"></div>
+  <form @submit.prevent="onSubmit">
+      <input type="email" v-model="email"><div class="break"></div>
+      <input type="password" v-model="password"><div class="break"></div>
+      <input type="password" v-model="confirm_password" v-if="isRegistration"><div class="break"></div>
       <button type="submit" class="button btn-submit">Зарегистрироваться</button>
   </form>
 </template>
 
 <script>
+import {firebase} from "../main"
+import router from '../router'
 export default {
-
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirm_password: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.password === this.confirm_password) {
+        firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        .catch(function(error) {
+          console.log(error.message)
+        })
+      }
+      this.email = this.password = this.confirm_password = ''
+      router.push('/')
+    }
+  }
 }
 </script>
 
@@ -43,7 +63,7 @@ form {
     color: $secondary;
 
     &:hover {
-      background-color: #413b63;
+      background-color: $highlight;
     }
   }
 
